c431f750e9fa2c6a9bc3db3142742bb204bea430
Daniil Shcherbakov <Charel@bk.ru>
2025-05-31 10:08:32 +0300

    Initial commit

---
+++ src/SearchInsertPosition_74.kt
@@ -0,0 +1,36 @@
+
+
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return
+ the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4 */
+
+
+fun searchInsert(nums: IntArray, target: Int): Int {
+    var left = 0
+    var right = nums.size-1
+
+    while (left <= right){
+        var index = left + (right - left) /2
+        if (nums[index] == target) return index
+        if (nums[index] < target){
+            left = index + 1
+        } else right = index - 1
+    }
+   return left
+}
